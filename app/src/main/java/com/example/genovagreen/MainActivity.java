962e7f28925c05e5f4bae3550895ea8c93c6c963
Luca Rota <lucarota981@gmail.com>
2020-12-06 15:55:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/genovagreen/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.genovagreen;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawer, toolbar,
+        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new ContentFragment()).commit();
+        navigationView.setCheckedItem(R.id.content_main);
+        }
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.content_main:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContentFragment()).commit();
+                break;
+            case R.id.dove_lo_butto:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ButtoFragment()).commit();
+                break;
+            case R.id.pericolosi:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PericolosiFragment()).commit();
+                break;
+            case R.id.spedizioni:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SpedizioniFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
