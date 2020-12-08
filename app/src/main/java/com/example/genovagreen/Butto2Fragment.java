8a988102ee15f8eace05b7b751e07586fdc27f43
Andrea <cappelli.andrea.1998@gmail.com>
2020-12-08 12:12:01 +0100

    aggiungi bottone in fragment_butto.xml e fragment_butto2.xml

--- app/src/main/java/com/example/genovagreen/Butto2Fragment.java
+++ app/src/main/java/com/example/genovagreen/Butto2Fragment.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
@@ -13,11 +14,25 @@ import java.sql.Connection;
 public class Butto2Fragment extends Fragment {
     Connection conn=MainActivity.StabilireConnessione();
     SearchView trova=ButtoFragment.ricerca;
+    Button cerca;
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //getSupportFragmentManager mi da errore
+            ButtoFragment.ricerca=trova;
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Butto2Fragment()).commit();
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+          View view=inflater.inflate(R.layout.fragment_butto2, container,  false);
+          cerca=(Button) view.findViewById(R.id.button);
+          trova=(SearchView view.findViewById(R.id.searchView);
 
-        return inflater.inflate(R.layout.fragment_butto2, container,  false);
+        return view;
     }
 }
