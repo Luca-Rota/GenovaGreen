a480e0adcd429f0ee8b682ea163dc6d24dc3d5c8
Luca Rota <lucarota981@gmail.com>
2020-12-12 13:00:12 +0100

    dio

---
+++ app/src/main/java/com/example/genovagreen/ContattiFragment.java
@@ -0,0 +1,33 @@
+package com.example.genovagreen;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Locale;
+
+public class ContattiFragment extends Fragment {
+    private int checkedItem=0;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_contatti, container,  false);
+        return view;
+    }
+
+
+}
