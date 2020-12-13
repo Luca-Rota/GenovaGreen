c53f88519674a58ab89fc10bf13b26d65f361b96
Andrea <cappelli.andrea.1998@gmail.com>
2020-12-13 10:08:35 +0100

    aaa

--- app/src/main/java/com/example/genovagreen/AdapterClass.java
+++ app/src/main/java/com/example/genovagreen/AdapterClass.java
@@ -3,6 +3,8 @@ package com.example.genovagreen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,23 +12,31 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder> {
-    ArrayList<Oggetto> list;
+public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder> implements Filterable {
+    private List<Oggetto> list;
+    private List<Oggetto> listFull;
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
         TextView rifiuto, cassonetto;
 
-        public MyViewHolder(@NonNull View itemView){
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            rifiuto=itemView.findViewById(R.id.rifiutoId);
-            cassonetto=itemView.findViewById(R.id.cassonettoId);
+            rifiuto = itemView.findViewById(R.id.rifiutoId);
+            cassonetto = itemView.findViewById(R.id.cassonettoId);
         }
     }
-
-    public AdapterClass(ArrayList<Oggetto> list){
+    AdapterClass(List<Oggetto> list){
         this.list=list;
-    }
+        listFull=new ArrayList<>(list);
+}
+
         @NonNull
         @Override
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -36,8 +46,9 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder
 
         @Override
         public void onBindViewHolder( MyViewHolder myViewHolder, int i) {
-            myViewHolder.rifiuto.setText(list.get(i).getRifiuto());
-            myViewHolder.cassonetto.setText(list.get(i).getCassonetto());
+        Oggetto oggettoAttuale= list.get(i);
+            myViewHolder.rifiuto.setText(oggettoAttuale.getRifiuto());
+            myViewHolder.cassonetto.setText(oggettoAttuale.getCassonetto());
 
 
         }
@@ -47,6 +58,31 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder
             return list.size();
         }
 
-
+        private Filter filter=new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                List<Oggetto> filterList = new ArrayList<>();
+                if (constraint == null || constraint.length() == 0) {
+                    filterList.addAll(listFull);
+                }
+                else{
+                    String pattrn=constraint.toString().toLowerCase().trim();
+                    for(Oggetto ogg:listFull){
+                        if(ogg.getRifiuto().toLowerCase().contains(pattrn)){
+                            filterList.add(ogg);
+                        }
+                    }
+            }
+                FilterResults filterResults=new FilterResults();
+                filterResults.values=filterList;
+                return filterResults;
+        }
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+              list.clear();
+              list.addAll((List)results.values);
+              notifyDataSetChanged();
+            }
+        };
     }
 
--- app/src/main/java/com/example/genovagreen/ButtoFragment.java
+++ app/src/main/java/com/example/genovagreen/ButtoFragment.java
@@ -3,6 +3,9 @@ package com.example.genovagreen;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
@@ -22,81 +25,86 @@ import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ButtoFragment extends Fragment {
+    private AdapterClass adapter;
+    private List<Oggetto> list = new ArrayList<>();
     DatabaseReference ref;
-    ArrayList<Oggetto> list;
-    RecyclerView recyclerView;
-    SearchView searchView;
+    View view=null;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        ref= FirebaseDatabase.getInstance().getReference().child("genovagreen-e27e2-default-rtdb").child("DoveLoButto");
-        View view=inflater.inflate(R.layout.fragment_butto, container,  false);
-        recyclerView=view.findViewById(R.id.rv);
-        recyclerView.hasFixedSize();
-        searchView=view.findViewById(R.id.searchView);
+        view=inflater.inflate(R.layout.fragment_butto, container,  false);
+        fillList();
     return view;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        recyclerView = view.findViewById(R.id.rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        list = new ArrayList<Oggetto>();
-        recyclerView.setAdapter(new AdapterClass(list));
+    private void fillList() {
+        for(int i=0;i<100000;i++) {
+            String rif="rifiuto"+i;
+            String cas="cassonetto"+i;
+            Oggetto ogg = new Oggetto(rif, cas);
+            list.add(ogg);
+        }
+        RecyclerView recyclerView = view.findViewById(R.id.rv);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        adapter = new AdapterClass(list);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        ref=FirebaseDatabase.getInstance().getReference().child("genovagreen-e27e2-default-rtdb/DoveLoButto");
+        ref.addValueEventListener(new ValueEventListener() {
+            //Non entra qua
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    String rifiuto=ds.child("rifiuto").getValue().toString();
+                    String cassonetto=ds.child("cassonetto").getValue().toString();
+                    Oggetto ogg=new Oggetto(rifiuto, cassonetto);
+                    list.add(ogg);
+                    Toast.makeText(getContext(),"fillList",Toast.LENGTH_LONG).show();
+                }
+                RecyclerView recyclerView=view.findViewById(R.id.rv);
+                recyclerView.setHasFixedSize(true);
+                RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext());
+                adapter=new AdapterClass(list);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+                Toast.makeText(getContext(),"adapter settato",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(),"errore db",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        if(ref!=null){
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.exists()){
-                        list=new ArrayList<Oggetto>();
-                        for(DataSnapshot ds: snapshot.getChildren()){
-                            list.add(ds.getValue(Oggetto.class));
-                        }
-                        AdapterClass adapterClass= new AdapterClass(list);
-                        recyclerView.setAdapter(adapterClass);
-                    }
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Toast.makeText(getContext(),error.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        if(searchView!=null){
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String s) {
-                    return false;
-                }
-
-                @Override
-                public boolean onQueryTextChange(String s) {
-                    search(s);
-                    return false;
-                }
-            });
-        }
+        search();
     }
-    private void search(String str){
-        ArrayList<Oggetto> myList= new ArrayList<Oggetto>();
-        for(Oggetto object:list){
-            if(object.getRifiuto().toLowerCase().contains(str.toLowerCase())){
-                myList.add(object);
+
+    public boolean search() {
+        SearchView searchView=view.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
             }
-        }
-        AdapterClass adapterClass=new AdapterClass(myList);
-        recyclerView.setAdapter(adapterClass);
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return true;
     }
 
 }
