2d5467fa63e94e349793b939dc5f86d41b338c37
Luca Rota <lucarota981@gmail.com>
2021-03-25 17:09:59 +0100

    aaaaaaaaaah

--- app/src/main/java/com/example/genovagreen/PericolosiFragment.java
+++ app/src/main/java/com/example/genovagreen/PericolosiFragment.java
@@ -1,23 +1,43 @@
 package com.example.genovagreen;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
 public class PericolosiFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    private Button but;
 
-        return inflater.inflate(R.layout.fragment_pericolosi, container,  false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pericolosi, container, false);
+        but = view.findViewById(R.id.button3);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
+                gotoUrl("https://goo.gl/maps/Xapa92hQ32sktxBQ9");
+            }
+        });
+        return view;
     }
 
+    
 
+    private void gotoUrl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW.uri));
+    }
 }
