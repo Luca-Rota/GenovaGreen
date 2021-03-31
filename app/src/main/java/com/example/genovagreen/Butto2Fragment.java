be64e46fdbec693512e8f8f42534bf7eb86d77de
Andrea <cappelli.andrea.1998@gmail.com>
2021-03-31 13:09:52 +0200

    aaa

--- app/src/main/java/com/example/genovagreen/Butto2Fragment.java
+++ app/src/main/java/com/example/genovagreen/Butto2Fragment.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,25 +13,58 @@ import androidx.fragment.app.Fragment;
 import java.sql.Connection;
 
 public class Butto2Fragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
+    private String mParam1;
+    private String mParam2;
+    String rifiuto, cassonetto, descrizione;
+    public Butto2Fragment() {
 
-
-    View.OnClickListener buttonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //getSupportFragmentManager mi da errore
-
-            getFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new Butto2Fragment()).commit();
-        }
-    };
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-          View view=inflater.inflate(R.layout.fragment_butto2, container,  false);
-
-        return view;
     }
+
+    public Butto2Fragment(String rifiuto, String cassonetto, String descrizione) {
+        this.rifiuto=rifiuto;
+        this.cassonetto=cassonetto;
+        this.descrizione=descrizione;
+    }
+
+    public static Butto2Fragment newInstance(String param1, String param2) {
+        Butto2Fragment fragment = new Butto2Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_butto2, container, false);
+
+        TextView rifiutoholder=view.findViewById(R.id.rifiutoholder);
+        TextView cassonettoholder=view.findViewById(R.id.cassonettoholder);
+        TextView descrizioneholder=view.findViewById(R.id.descrizioneholder);
+
+
+        rifiutoholder.setText(rifiuto);
+        cassonettoholder.setText(cassonetto);
+        descrizioneholder.setText(descrizione);
+
+
+
+        return  view;
+    }
+
 }
