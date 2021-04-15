543e970c021ab85e5245a37413458903d9e7e2a6
Andrea <cappelli.andrea.1998@gmail.com>
2021-04-15 19:30:17 +0200

    aaa

--- app/src/main/java/com/example/genovagreen/Spedizioni4.java
+++ app/src/main/java/com/example/genovagreen/Spedizioni4.java
@@ -57,7 +57,7 @@ import java.util.List;
 import java.util.Locale;
 
 
-public class Spedizioni4 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+public class Spedizioni4 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private FirebaseAuth auth;
     private DrawerLayout drawer;
@@ -69,9 +69,6 @@ public class Spedizioni4 extends AppCompatActivity implements NavigationView.OnN
     private Button map, agg;
     private TextView mapText;
     private String date, time;
-    private GoogleMap mMap;
-    private final static int PLACE_PICKER_REQUEST = 999;
-    private final static int LOCATION_REQUEST_CODE = 23;
     private EditText descrizione4;
     private DatabaseReference ref;
     private String nomeutente;
@@ -156,17 +153,8 @@ public class Spedizioni4 extends AppCompatActivity implements NavigationView.OnN
         map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MapFragment fragment = new MapFragment();
-                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                getSupportFragmentManager().beginTransaction().replace(R.id.wrapper1, fragment).commit();
-                SupportMapFragment mapFragment = new SupportMapFragment();
-                mapFragment.getMapAsync((OnMapReadyCallback) v.getContext());
-                if (ContextCompat.checkSelfPermission(v.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(Spedizioni4.this, MapSpedizioni.class));
 
-                    ActivityCompat.requestPermissions((Activity) v.getContext(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
-                            LOCATION_REQUEST_CODE);
-                }
             }
         });
         position4=findViewById(R.id.position);
@@ -179,10 +167,14 @@ public class Spedizioni4 extends AppCompatActivity implements NavigationView.OnN
         agg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String partecipanti="1";
-                Spedizione spedizione=new Spedizione(posizione,descrizione,nomeutente,date,ora,partecipanti);
-                ref= FirebaseDatabase.getInstance().getReference("Spedizioni").push();
-                ref.setValue(spedizione);
+                if(!descrizione.isEmpty()&&!posizione.isEmpty()&&!ora.isEmpty()&&!date.isEmpty()) {
+                    String partecipanti = "1";
+                    Spedizione spedizione = new Spedizione(posizione, descrizione, nomeutente, date, ora, partecipanti);
+                    ref = FirebaseDatabase.getInstance().getReference("Spedizioni").push();
+                    ref.setValue(spedizione);
+                }else{
+                    Toast.makeText(Spedizioni4.this,"Tutti i campi devono essere riempiti", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -252,105 +244,5 @@ public class Spedizioni4 extends AppCompatActivity implements NavigationView.OnN
         return true;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case LOCATION_REQUEST_CODE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
-                    mMap.setMyLocationEnabled(true);
-                    mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-                        @Override
-                        public void onMyLocationChange(Location location) {
-                            LatLng ltlng=new LatLng(location.getLatitude(),location.getLongitude());
-                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
-                                    ltlng, 16f);
-                            mMap.animateCamera(cameraUpdate);
-                        }
-                    });
-                    Location location = mMap.getMyLocation();
-
-                    mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                        @Override
-                        public void onMapClick(LatLng latLng) {
-                            MarkerOptions markerOptions = new MarkerOptions();
-                            markerOptions.position(latLng);
-
-                            markerOptions.title(getAddress(latLng));
-                            mMap.clear();
-                            CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
-                                    latLng, 15);
-                            mMap.animateCamera(location);
-                            mMap.addMarker(markerOptions);
-                        }
-                    });
-
-
-
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-        }
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-    }
-
-
-    private String getAddress(LatLng latLng){
-
-        Geocoder geocoder;
-        List<Address> addresses;
-        geocoder = new Geocoder(this, Locale.getDefault());
-
-        try {
-            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-            String city = addresses.get(0).getLocality();
-            String state = addresses.get(0).getAdminArea();
-            String country = addresses.get(0).getCountryName();
-            String postalCode = addresses.get(0).getPostalCode();
-            String knownName = addresses.get(0).getFeatureName();
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            Fragment prev = getFragmentManager().findFragmentByTag("dialog");
-            if (prev != null) {
-
-                ft.remove(prev);
-            }
-            ft.addToBackStack(null);
-            DialogFragment dialogFragment = new ConfirmAddress();
-
-            Bundle args = new Bundle();
-            args.putDouble("lat", latLng.latitude);
-            args.putDouble("long", latLng.longitude);
-            args.putString("address", address);
-            dialogFragment.setArguments(args);
-            dialogFragment.show(ft, "dialog");
-            return address;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "No Address Found";
-
-        }
-
-
-    }
 }
